484cc89a44fa07157dd555d17694e6f8cb90244a
stevecarvalho <151648920+stevecarvalho@users.noreply.github.com>
2024-03-21 17:21:24 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Leaflet Map
+
+const map = L.map('map').setView([44.837522, -0.547452], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
